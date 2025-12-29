778345b686fb3c3edc541a77f962bc539cc2b63a
ConectadoemConcursos <conectadoemconcursos@gmail.com>
2025-12-29 00:45:12 +0000

    File changes

--- src/pages/ComoEstudarPrimeiroLugar.jsx
+++ src/pages/ComoEstudarPrimeiroLugar.jsx
@@ -16,7 +16,7 @@ export default function ComoEstudarPrimeiroLugar() {
   const [editMode, setEditMode] = useState(false);
 
   const extractYouTubeId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
   };
--- src/pages/GuiaEstudos.jsx
+++ src/pages/GuiaEstudos.jsx
@@ -21,7 +21,7 @@ export default function GuiaEstudos() {
   const [editMode, setEditMode] = useState(false);
 
   const extractYouTubeId = (url) => {
-    const regExp = /^.*(youtu.be\\/|v\\/|u\\/\\w\\/|embed\\/|watch\\?v=|&v=)([^#&?]*).*/;
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return match && match[2].length === 11 ? match[2] : null;
   };
