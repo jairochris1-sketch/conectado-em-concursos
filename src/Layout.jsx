54f202ffca9a357712e42c8a56af1c131c588e88
ConectadoemConcursos <conectadoemconcursos@gmail.com>
2026-01-18 17:07:40 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -157,6 +157,8 @@ const featureAccess = {
   'Minhas Anotações': ['padrao', 'avancado'],
   'Simulados Digital': ['padrao', 'avancado'],
   'Resumos': ['avancado'],
+  'Provas': ['gratuito', 'padrao', 'avancado'],
+  'Visualização de Prova': ['gratuito', 'padrao', 'avancado'],
 };
 
 const checkAccess = (featureTitle, plan, isAdmin) => {
