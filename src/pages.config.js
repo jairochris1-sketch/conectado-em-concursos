4b1fc714e016a5f4b65018ac0c95b199fb44d767
ConectadoemConcursos <conectadoemconcursos@gmail.com>
2026-01-19 01:29:56 +0000

    ArticleManager Completo e Funcional

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,5 +1,7 @@
+import Admin from './pages/Admin';
 import AssistirAula from './pages/AssistirAula';
 import ChatGPT from './pages/ChatGPT';
+import ComoEstudarPrimeiroLugar from './pages/ComoEstudarPrimeiroLugar';
 import Dashboard from './pages/Dashboard';
 import DigitalWhiteboard from './pages/DigitalWhiteboard';
 import ExamView from './pages/ExamView';
@@ -23,14 +25,14 @@ import Welcome from './pages/Welcome';
 import chatgpt from './pages/chatgpt';
 import studies from './pages/studies';
 import subscription from './pages/subscription';
-import Admin from './pages/Admin';
-import ComoEstudarPrimeiroLugar from './pages/ComoEstudarPrimeiroLugar';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
+    "Admin": Admin,
     "AssistirAula": AssistirAula,
     "ChatGPT": ChatGPT,
+    "ComoEstudarPrimeiroLugar": ComoEstudarPrimeiroLugar,
     "Dashboard": Dashboard,
     "DigitalWhiteboard": DigitalWhiteboard,
     "ExamView": ExamView,
@@ -54,8 +56,6 @@ export const PAGES = {
     "chatgpt": chatgpt,
     "studies": studies,
     "subscription": subscription,
-    "Admin": Admin,
-    "ComoEstudarPrimeiroLugar": ComoEstudarPrimeiroLugar,
 }
 
 export const pagesConfig = {
