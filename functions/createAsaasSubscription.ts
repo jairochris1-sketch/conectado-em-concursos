4d2364b93c8558dff5848732427ef8f514207576
ConectadoemConcursos <conectadoemconcursos@gmail.com>
2026-01-19 03:53:35 +0000

    File changes

--- functions/createAsaasSubscription.ts
+++ functions/createAsaasSubscription.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.7.1';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 Deno.serve(async (req) => {
     try {
@@ -191,7 +191,6 @@ Deno.serve(async (req) => {
 
             return Response.json({
                 success: true,
-                subscription_id: subscription.id,
                 payment_url: firstPayment.invoiceUrl
             });
 
