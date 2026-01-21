4f2be1e47409a71804c54d0faf2f0bd4d7c0e7a7
Jairo Dos Santos <jairochris1@gmail.com>
2026-01-21 01:09:19 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -293,7 +293,7 @@ export default function Layout({ children, currentPageName }) {
           const now = new Date();
           const diffTime = now.getTime() - trialStartDate.getTime();
           const diffDays = diffTime / (1000 * 60 * 60 * 24);
-          const trialDuration = 10;
+          const trialDuration = 5;
           const daysRemaining = trialDuration - diffDays;
 
           if (daysRemaining <= 0) {
