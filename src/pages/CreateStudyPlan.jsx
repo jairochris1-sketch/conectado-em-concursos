c8f67ab219e5641cda4744eb4af368640d92a71f
Jairo Dos Santos <jairochris1@gmail.com>
2026-01-21 20:42:28 +0000

    File changes

--- src/pages/CreateStudyPlan.jsx
+++ src/pages/CreateStudyPlan.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { User, StudyPlan, UserExamDate } from '@/entities/all';
+import { User, StudyPlan, UserExamDate, UserAnswer } from '@/entities/all';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
