7b5ad025ac3b6a69dfad340dd161faa4d8df820b
ConectadoemConcursos <conectadoemconcursos@gmail.com>
2026-03-05 19:01:44 +0000

    File changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -367,6 +367,14 @@ export default function ProfilePage() {
               </Card>
             </motion.div>
 
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.05 }}
+            >
+              <BadgesCard user={user} />
+            </motion.div>
+
             {/* Informações pessoais */}
             <motion.div
               initial={{ opacity: 0, y: 20 }}
